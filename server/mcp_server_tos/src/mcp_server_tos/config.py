801632236252409e796c33aef4601bb296f4e114
zhoudinghua <zhoudinghua@bytedance.com>
2025-05-15 19:33:31 +0800

    feat:TOS支持Remote MCP

--- server/mcp_server_tos/src/mcp_server_tos/config.py
+++ server/mcp_server_tos/src/mcp_server_tos/config.py
@@ -1,10 +1,13 @@
 import logging
 import os
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
+
 from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
+LOCAL_DEPLOY_MODE = "local"
+REMOTE_DEPLOY_MODE = "remote"
 
 
 @dataclass
@@ -16,17 +19,21 @@ class TosConfig:
         VOLC_SECRETKEY: Access key secret for authentication
         TOS_ENDPOINT:   The TOS service endpoint
         REGION:         The region of the TOS service
+        DEPLOY_MODE:    The deployment mode
         TOS_BUCKETS:    The bucket list to use for the TOS service
+        MAX_OBJECT_SIZE: The maximum size of an object in bytes
     """
     access_key: str
     secret_key: str
     region: str
     endpoint: str
     security_token: str
+    deploy_mode: str
+    max_object_size: int
     buckets: List[str]
 
 
-def validate_required_vars():
+def validate_local_required_vars():
     """
     Validate that all required environment variables are set.
 
@@ -42,15 +49,38 @@ def validate_required_vars():
         raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
 
 
+def validate_remote_required_vars():
+    """
+    Validate that all required environment variables are set.
+
+    Raises:
+    ValueError: If any required environment variable is missing.
+    """
+    missing_vars = []
+    for var in ["TOS_ENDPOINT"]:
+        if var not in os.environ:
+            missing_vars.append(var)
+
+    if missing_vars:
+        raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
+
+
 def load_config() -> TosConfig:
-    validate_required_vars()
+    deploy_mode = os.getenv("DEPLOY_MODE", LOCAL_DEPLOY_MODE)
+    if deploy_mode == LOCAL_DEPLOY_MODE:
+        validate_local_required_vars()
+    else:
+        validate_remote_required_vars()
+
     config = TosConfig(
-        access_key=os.environ["VOLC_ACCESSKEY"],
-        secret_key=os.environ["VOLC_SECRETKEY"],
+        access_key=os.getenv("VOLC_ACCESSKEY", ""),
+        secret_key=os.getenv("VOLC_SECRETKEY", ""),
         region=os.getenv("REGION", ""),
         endpoint=os.environ["TOS_ENDPOINT"],
         security_token=os.getenv("SECURITY_TOKEN", ""),
+        deploy_mode=deploy_mode,
         buckets=os.getenv("TOS_BUCKETS", "").split(","),
+        max_object_size=int(os.getenv("MAX_OBJECT_SIZE", "262144")),
     )
     logger.info(f"Loaded configuration, endpoint: {config.endpoint}, region: {config.region} buckets: {config.buckets}")
 
@@ -58,3 +88,5 @@ def load_config() -> TosConfig:
 
 
 load_dotenv()
+
+TOS_CONFIG = load_config()
