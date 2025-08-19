71ef778650fa87e9eba593169ad171f0810ea43f
lailirong <lailirong@bytedance.com>
2025-08-19 17:16:06 +0800

    add more apis

--- server/mcp_server_cen/src/mcp_server_cen/base/cen.py
+++ server/mcp_server_cen/src/mcp_server_cen/base/cen.py
@@ -5,7 +5,16 @@ from volcenginesdkcen.api.cen_api import CENApi
 from volcenginesdkcen.models import DescribeCensRequest, DescribeCensResponse, \
         DescribeCenAttributesRequest, DescribeCenAttributesResponse, \
         DescribeInstanceGrantedRulesRequest, DescribeInstanceGrantedRulesResponse, \
-        DescribeGrantRulesToCenRequest, DescribeGrantRulesToCenResponse
+        DescribeGrantRulesToCenRequest, DescribeGrantRulesToCenResponse, \
+        DescribeCenAttachedInstanceAttributesRequest, DescribeCenAttachedInstanceAttributesResponse, \
+        DescribeCenAttachedInstancesRequest, DescribeCenAttachedInstancesResponse, \
+        DescribeCenBandwidthPackagesRequest, DescribeCenBandwidthPackagesResponse, \
+        DescribeCenBandwidthPackageAttributesRequest, DescribeCenBandwidthPackageAttributesResponse, \
+        DescribeCenInterRegionBandwidthAttributesRequest, DescribeCenInterRegionBandwidthAttributesResponse, \
+        DescribeCenInterRegionBandwidthsRequest, DescribeCenInterRegionBandwidthsResponse, \
+        DescribeCenServiceRouteEntriesRequest, DescribeCenServiceRouteEntriesResponse, \
+        DescribeCenRouteEntriesRequest, DescribeCenRouteEntriesResponse, \
+        DescribeCenSummaryRouteEntriesRequest, DescribeCenSummaryRouteEntriesResponse
 
 
 class CENSDK:
@@ -31,3 +40,33 @@ class CENSDK:
 
     def describe_grant_rules_to_cen(self, args: dict) -> DescribeGrantRulesToCenResponse:
         return self.client.describe_grant_rules_to_cen(DescribeGrantRulesToCenRequest(**args))
+    
+    def describe_cen_attached_instance_attributes(self, args: dict) -> DescribeCenAttachedInstanceAttributesResponse:
+        return self.client.describe_cen_attached_instance_attributes(DescribeCenAttachedInstanceAttributesRequest(**args))
+    
+    def describe_cen_attached_instances(self, args: dict) -> DescribeCenAttachedInstancesResponse:
+        return self.client.describe_cen_attached_instances(DescribeCenAttachedInstancesRequest(**args))
+    
+    def describe_cen_bandwidth_packages(self, args: dict) -> DescribeCenBandwidthPackagesResponse:
+        return self.client.describe_cen_bandwidth_packages(DescribeCenBandwidthPackagesRequest(**args))
+    
+    def describe_cen_bandwidth_package_attributes(self, args: dict) -> DescribeCenBandwidthPackageAttributesResponse:
+        return self.client.describe_cen_bandwidth_package_attributes(DescribeCenBandwidthPackageAttributesRequest(**args))
+    
+    # def describe_cen_bandwidth_packages_billing(self, args: dict) -> DescribeCenBandwidthPackagesBillingResponse:
+    #     return self.client.describe_cen_bandwidth_packages_billing(DescribeCenBandwidthPackagesBillingRequest(**args))
+    
+    def describe_cen_inter_region_bandwidth_attributes(self, args: dict) -> DescribeCenInterRegionBandwidthAttributesResponse:
+        return self.client.describe_cen_inter_region_bandwidth_attributes(DescribeCenInterRegionBandwidthAttributesRequest(**args))
+    
+    def describe_cen_inter_region_bandwidths(self, args: dict) -> DescribeCenInterRegionBandwidthsResponse:
+        return self.client.describe_cen_inter_region_bandwidths(DescribeCenInterRegionBandwidthsRequest(**args))
+    
+    def describe_cen_service_route_entries(self, args: dict) -> DescribeCenServiceRouteEntriesResponse:
+        return self.client.describe_cen_service_route_entries(DescribeCenServiceRouteEntriesRequest(**args))
+    
+    def describe_cen_route_entries(self, args: dict) -> DescribeCenRouteEntriesResponse:
+        return self.client.describe_cen_route_entries(DescribeCenRouteEntriesRequest(**args))
+    
+    def describe_cen_summary_route_entries(self, args: dict) -> DescribeCenSummaryRouteEntriesResponse:
+        return self.client.describe_cen_summary_route_entries(DescribeCenSummaryRouteEntriesRequest(**args))
