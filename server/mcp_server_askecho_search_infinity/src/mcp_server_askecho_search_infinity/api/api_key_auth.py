85d2e106a953b4537129ac1fab4060e4262ce717
Marongkun <marongkun.kwn@bytedance.com>
2025-09-16 17:06:10 +0800

    feat: search_infinity async

--- server/mcp_server_askecho_search_infinity/src/mcp_server_askecho_search_infinity/api/api_key_auth.py
+++ server/mcp_server_askecho_search_infinity/src/mcp_server_askecho_search_infinity/api/api_key_auth.py
@@ -1,22 +1,24 @@
 from dataclasses import asdict
 import json
-import requests
+import aiohttp
 from ..model import *
 
 Host = "open.feedcoopapi.com"
 ContentType = "application/json"
 
 
-def web_search_api_key_auth(api_key: str, req: WebSearchRequest, tool_name: str):
+async def web_search_api_key_auth(api_key: str, req: WebSearchRequest, tool_name: str):
     header = {
         "Content-Type": ContentType,
         "Authorization": f"Bearer {api_key}",
         "X-Traffic-Tag": f"ark_mcp_server_{tool_name}",
     }
-    r = requests.request(method="POST",
-                         url="https://{}{}".format(Host, "/search_api/web_search"),
-                         headers=header,
-                         timeout=3000,
-                         data=json.dumps(asdict(req))
-                         )
-    return r
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=f"https://{Host}/search_api/web_search",
+            headers=header,
+            timeout=aiohttp.ClientTimeout(total=3000),
+            data=json.dumps(asdict(req))
+        ) as response:
+            return response
